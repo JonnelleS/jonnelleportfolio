8858dd800476e592f616dbf07df269ada1c500a0
Jonnelle Sayles <jonnellesayles@gmail.com>
2017-07-20 22:52:11 -0400

    Create project.js

---
+++ project.js
@@ -0,0 +1,23 @@
+$("#2truths").click(function() {
+    $("#2truths").html("https://jonnelles.github.io/jonnelleportfolio/2truths.html");
+});
+
+$("#jokes").click(function() {
+    $("jokes").html("https://jonnelles.github.io/jonnelleportfolio/jokestyle.html");
+});
+
+$("#hack1").click(function() {
+    $("#hack1").html("https://jonnelles.github.io/jonnelleportfolio/hackathonone.html");
+});
+
+$("#hack2").click(function() {
+    $("#hack2").html("https://jonnelles.github.io/jonnelleportfolio/Hackathon2.html");
+});
+
+$("#2truths").click(function() {
+    $("#2truths").html("https://jonnelles.github.io/jonnelleportfolio/2truths.html");
+});
+
+$("#2truths").click(function() {
+    $("#2truths").html("https://jonnelles.github.io/jonnelleportfolio/2truths.html");
+});
